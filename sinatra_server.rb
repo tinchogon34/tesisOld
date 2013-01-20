f675ea069ae8a7b03c149f2c2a0160f1715c7399
Martin Gonzalez <tinchogon34@gmail.com>
2013-01-20 19:23:45 -0300

    Reindent

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require 'json'
 require 'uglifier'
-require 'ostruct'
-
 
 Vars = {
 	workers: [{nombre: 'worker1', data: [1,2,3,4,5,6,7,8,9,10] },
@@ -10,16 +8,6 @@ Vars = {
 	reduce_data: [],
 	current_worker: ''
 }
-
-	#Vars = OpenStruct.new(
-	#	workers: OpenStruct.new(worker1: OpenStruct.new(numeros: [1,2,3,4,5,6,7,8,9,10]),
-	#	           worker2: OpenStruct.new(numeros: [10,20,30,40,50])
-#
-#		 ),
-#		current_worker: '',
-#		reduce_data: []
-#	)
-
 
 get '/' do
 	send_file 'views/index.html'
@@ -58,36 +46,33 @@ post '/data' do
 		if Vars[:workers].empty?
 			return { type: 'finish' }.to_json
 		else
-
 			Vars[:reduce_data] = []
 			Vars[:current_worker] = Vars[:workers].pop
 			@data = { type: 'new_worker',
-		 		numeros: Vars[:current_worker][:data].slice!(0,2),
+				numeros: Vars[:current_worker][:data].slice!(0,2),
 				worker: Uglifier.compile(File.read("#{Vars[:current_worker][:nombre]}.js"))
-			}.to_json
+				}.to_json
 
-			puts @data
-			@data
+				puts @data
+				@data
 		end
-
 	else
 		@data = { type: 'new_data',
 			numeros: Vars[:current_worker][:data].slice!(0,2) 
 			}.to_json
-			logger.info @data
-			return @data
-		end
+		return @data
 	end
+end
 
-	post '/worker' do
-	end
+post '/worker' do
+end
 
-	post '/log' do
-		response['Content-Type'] = 'text/plain'
-		response['Access-Control-Allow-Origin'] = '*'
-		response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-		response['Access-Control-Max-Age'] = '1000'
-		response['Access-Control-Allow-Headers'] = 'Content-Type'
+post '/log' do
+	response['Content-Type'] = 'text/plain'
+	response['Access-Control-Allow-Origin'] = '*'
+	response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	response['Access-Control-Max-Age'] = '1000'
+	response['Access-Control-Allow-Headers'] = 'Content-Type'
 
-		"thx"
-	end
+	puts params[:data][:message]
+end
